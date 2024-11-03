f5e3bf6dd3b860185d55d6355a79ba01f30216d4
idpan <idpanashari813@gmail.com>
2024-11-03 23:55:29 +0700

    --feat:add content

--- src/pages/DataIzin.jsx
+++ src/pages/DataIzin.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import TableWrapper from "../components/TableWrapper";
 
 function DataIzin() {
-  return <div>DataIzin</div>;
+  const dataAbsensi = [
+    {
+      nama: "Yovan Fikri",
+      izin: "Pulang Lebih Awal",
+      tanggal: "2023-05-10",
+      jam: "16:21:14",
+      keterangan: "Sakit",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      izin: "Datang Terlambat",
+      tanggal: "2023-05-10",
+      jam: "16:21:14",
+      keterangan: "Macet",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      izin: "Datang Terlambat",
+      tanggal: "2023-05-10",
+      jam: "16:21:14",
+      keterangan: "Macet",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      izin: "Datang Terlambat",
+      tanggal: "2023-05-10",
+      jam: "16:21:14",
+      keterangan: "Macet",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      izin: "Datang Terlambat",
+      tanggal: "2023-05-10",
+      jam: "16:21:14",
+      keterangan: "Macet",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Renate Nindya",
+      izin: "Datang Terlambat",
+      tanggal: "2023-05-10",
+      jam: "16:21:14",
+      keterangan: "Sakit",
+      status: "Disetujui",
+    },
+  ];
+
+  return (
+    <div>
+      <div style={{ marginBottom: "-30px" }} className="hero">
+        <div className="container px-5 pt-5 pb-4"></div>
+      </div>
+      <TableWrapper
+        title="Daftar Hadir Karyawan"
+        btnLabel="Ubah Status"
+        isBtnStatus={true}
+        tHead={["No", "Nama", "Izin", "Tanggal", "Jam", "Keterangan", "Status"]}
+      >
+        {dataAbsensi.map((data, index) => {
+          return (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{data.nama}</td>
+              <td>{data.izin}</td>
+              <td>{data.tanggal}</td>
+              <td>{data.jam}</td>
+              <td>{data.keterangan}</td>
+              <td>{data.status}</td>
+            </tr>
+          );
+        })}
+      </TableWrapper>
+    </div>
+  );
 }
 
 export default DataIzin;
--- src/pages/DataLembur.jsx
+++ src/pages/DataLembur.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import TableWrapper from "../components/TableWrapper";
 
 function DataLembur() {
-  return <div>DataLembur</div>;
+  const dataAbsensi = [
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Disetujui",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Disetujui",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Disetujui",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Menunggu Konfirmasi",
+    },
+    {
+      nama: "Yovan Fikri",
+      tanggal: "2023-05-10",
+      jam: "16:21:14 | 17:00:00",
+      keterangan: "Lagi Rajin Aja",
+      status: "Menunggu Konfirmasi",
+    },
+  ];
+
+  return (
+    <div>
+      <div style={{ marginBottom: "-30px" }} className="hero">
+        <div className="container px-5 pt-5 pb-4"></div>
+      </div>
+      <TableWrapper
+        title="Daftar Hadir Karyawan"
+        btnLabel="Ubah Status"
+        isBtnStatus={true}
+        tHead={["No", "Nama", "Izin", "Tanggal", "Jam", "Keterangan", "Status"]}
+      >
+        {dataAbsensi.map((data, index) => {
+          return (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{data.nama}</td>
+              <td>{data.izin}</td>
+              <td>{data.tanggal}</td>
+              <td>{data.jam}</td>
+              <td>{data.keterangan}</td>
+              <td>{data.status}</td>
+            </tr>
+          );
+        })}
+      </TableWrapper>
+    </div>
+  );
 }
 
 export default DataLembur;
